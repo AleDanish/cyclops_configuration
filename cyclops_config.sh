af856c7b1cf78fe2d761af3147edd5f7ded4b837
Alessandro <alessandrozanni.dev@gmail.com>
2016-02-09 12:20:04 +0100

    edit a billing conf file

--- cyclops_config.sh
+++ cyclops_config.sh
@@ -16,7 +16,7 @@ sudo python string_substitution.py /home/ubuntu/cyclops-rc/src/main/webapp/WEB-I
 sudo python string_substitution.py /home/ubuntu/cyclops-rc/src/main/webapp/WEB-INF/configuration.txt UDRServiceUrl= http://$myip:8086/udr
 
 echo "-> Cyclops-billing configuration file"
-sudo python string_substitution.py /home/ubuntu/cyclops-billing/src/main/webapp/WEB-INF/configuration.txt InfluxDBURL= http://$myip:8086
+sudo python string_substitution.py /home/ubuntu/cyclops-billing/src/main/webapp/WEB-INF/configuration.txt 'RcServiceUrl==' http://$myip:8086/rc
 sudo python string_substitution.py /home/ubuntu/cyclops-udr/install/openstack/config/config.js url: ' "http://10.20.30.40:8086/db/udr_service",'
 
 echo "-> Finish setting up Cyclops!"
