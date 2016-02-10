7518d139ef95ad582779425f904d3f102891acb4
Alessandro <alessandrozanni.dev@gmail.com>
2016-02-10 01:06:24 +0100

    edit config

--- cyclops_config.sh
+++ cyclops_config.sh
@@ -13,10 +13,8 @@ sudo python string_substitution.py /home/ubuntu/cyclops-udr/src/main/webapp/WEB-
 
 echo "-> Cyclops-rc configuration file"
 sudo python string_substitution.py /home/ubuntu/cyclops-rc/src/main/webapp/WEB-INF/configuration.txt InfluxDBURL= http://$myip:8086
-sudo python string_substitution.py /home/ubuntu/cyclops-rc/src/main/webapp/WEB-INF/configuration.txt UDRServiceUrl= http://$myip:8080/udr
 
 echo "-> Cyclops-billing configuration file"
-sudo python string_substitution.py /home/ubuntu/cyclops-billing/src/main/webapp/WEB-INF/configuration.txt 'RcServiceUrl==' http://$myip:8080/rc
 sudo python string_substitution_js.py /home/ubuntu/cyclops-udr/install/openstack/config/config.js url: ' "http://'$myip':8086/db/udr_service",' ' "http://'$myip':8086/db/grafana",'
 
 echo "-> Finish setting up Cyclops!"
